b99e87886f3942fc5d96301153f093a01fea75cf
Nayab Ahmad <mdnayabahmad441@gmail.com>
2026-03-07 14:04:29 +0530

    fix nextauth redirect loop

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,25 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
   ],
-  secret: process.env.NEXTAUTH_SECRET,
-});
+  session: {
+    strategy: "jwt"
+  },
+  pages: {
+    signIn: "/login"
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return baseUrl + "/platform"
+    }
+  },
+  secret: process.env.NEXTAUTH_SECRET
+})
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
