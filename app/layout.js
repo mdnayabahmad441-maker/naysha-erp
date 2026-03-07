b638ea74a1e0e5a020104eec867ff26c490c4d28
Nayab Ahmad <mdnayabahmad441@gmail.com>
2026-03-07 14:00:46 +0530

    fix tailwind styling

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,14 @@
-"use client";
+import "./globals.css";
 
-import { SessionProvider } from "next-auth/react";
+export const metadata = {
+  title: "NaySha ERP",
+  description: "Smart School ERP Platform",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>
-        <SessionProvider>
-          {children}
-        </SessionProvider>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
